5a148bcf8b6ebcca10f680f90995ed8500bfd601
Chris Salzberg <chrissalzberg@gmail.com>
2011-11-11 16:42:35 +0900

    Added rails project files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yogoshu::Application.config.secret_token = 'bccf536dbb2466fc9127beeeec6ace8b1a53f86c118643363cf9ba5923ecee6d595281d3d07a62ab7d6c5e111b85972e9116ebb9f65df13139cce5fa52a83ecc'
