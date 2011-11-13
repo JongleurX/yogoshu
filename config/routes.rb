72b741bdeaed58f108c7c9c35b617e700b89bcc9
Chris Salzberg <chrissalzberg@gmail.com>
2011-11-13 18:10:06 +0900

    Added users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Yogoshu::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :users
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
