7b3ac26cf02510718825a76ca0c1a526673afb6a
Chris Salzberg <chrissalzberg@gmail.com>
2011-12-01 17:51:31 +0900

    Wording change to stay consistent.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,19 +11,19 @@ describe SessionsController do
       post :create, :user => { 'these' => 'params' }
     end
 
-    it "should assign @current_user" do
+    it "assigns @current_user" do
       assigns(:current_user).should be(@user)
     end
 
-    it "should set the session cookie" do
+    it "sets the session cookie" do
       session[:user].should == @user.name
     end
 
-    it "should redirect to the homepage" do
+    it "redirects to the homepage" do
       response.should redirect_to(homepage_path)
     end
 
-    it "should welcome the user back" do
+    it "welcomes the user back" do
       flash[:message].should include "Welcome back #{@user.name}!"
     end
 
@@ -36,19 +36,19 @@ describe SessionsController do
       post :create, :user => { 'these' => 'params' }
     end
 
-    it "should not assign @current_user" do
+    it "does not assign @current_user" do
       assigns(:current_user).should be_nil
     end
 
-    it "should not set the session cookie" do
+    it "does not set the session cookie" do
       session[:user].should be_nil
     end
 
-    it "should re-render the login page" do
+    it "re-renders the login page" do
       response.should render_template('new')
     end
 
-    it "should return an error message" do
+    it "returns an error message" do
       flash[:error].should == "Username or password incorrect. Please try again."
     end
 
@@ -60,19 +60,19 @@ describe SessionsController do
       post :destroy
     end
 
-    it "should unset the session cookie" do
+    it "unsets the session cookie" do
       session[:user].should be_nil
     end
 
-    it "should not set @current_user" do
+    it "does not set @current_user" do
       @current_user.should be_nil
     end
 
-    it "should redirect to the homepage" do
+    it "redirects to the homepage" do
       response.should redirect_to(homepage_path)
     end
 
-    it "should alert the user that they are logged out" do
+    it "alerts the user that they are logged out" do
       flash[:message].should include("logged out")
     end
 
