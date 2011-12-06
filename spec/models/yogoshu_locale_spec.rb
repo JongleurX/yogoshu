d3eda8271cec6eed17b6ed8934d47a4b6d9684f3
Chris Salzberg <chrissalzberg@gmail.com>
2011-12-06 21:07:27 +0900

    Added specs for Yogoshu::Locale.

---
+++ spec/models/yogoshu_locale_spec.rb
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe Yogoshu::Locale do
+
+  after do
+    I18n.locale = nil
+    Globalize.locale = nil
+    Yogoshu::Locale::set_base_languages(:ja, :en)
+  end
+
+  describe "#set_base_languages" do
+
+    it "raises an error for arguments that are not Symbols" do
+      expect { Yogoshu::Locale.set_base_languages(:en, "abc") }.to raise_error(ArgumentError)
+    end
+
+    it " assigns base_languages" do
+      Yogoshu::Locale.set_base_languages(:de, :ja)
+      Yogoshu::Locale.base_languages.should == [:de, :ja]
+    end
+
+  end
+
+  describe "#respond_to?" do
+
+    before { Yogoshu::Locale.set_base_languages(:ja, :en) }
+
+    it "returns true for translated attribute with postfix in base_languages" do
+      @entry = Entry.new
+      @entry.respond_to?(:term_in_en).should == true
+      @entry.respond_to?(:term_in_ja).should == true
+    end
+
+    it "returns false for translated attribute with postfix not in base_languages" do
+      @entry = Entry.new
+      @entry.respond_to?(:term_in_de).should == false
+    end
+
+  end
+
+  describe "#method_missing" do
+
+    before { Yogoshu::Locale.set_base_languages(:ja, :en) }
+
+    it "raises a NoMethodError unless postfix is a locale in base_languages" do
+      @entry = Entry.new
+      expect { @entry.term_in_ja }.not_to raise_error
+      expect { @entry.term_in_en }.not_to raise_error
+      expect { @entry.term_in_de }.to raise_error(NoMethodError)
+    end
+
+  end
+
+  describe "translated field access with postfix" do
+
+    context "2-languages(:ja, :en)" do
+
+      include Yogoshu::Locale
+
+      before do
+        Yogoshu::Locale::set_base_languages(:ja, :en)
+      end
+
+      context "given an entry with term 'RIKEN' in :en and '理化学研究所' in :ja" do
+
+        before do
+          @entry = Entry.new
+          Globalize.with_locale(:en) do
+            @entry.term = 'RIKEN'
+          end
+          Globalize.with_locale(:ja) do
+            @entry.term = '理化学研究所'
+          end
+        end
+
+        subject { @entry }
+
+        describe "getter functions for source_language 'ja'" do
+          before { subject.source_language = 'ja' }
+          its(:term_in_en) { should == 'RIKEN' }
+          its(:term_in_ja) { should == '理化学研究所' }
+          its(:term_in_source_language) { should == '理化学研究所' }
+        end
+
+        describe "getter functions for source_language 'en'" do
+          before { subject.source_language = 'en' }
+          its(:term_in_en) { should == 'RIKEN' }
+          its(:term_in_ja) { should == '理化学研究所' }
+          its(:term_in_source_language) { should == 'RIKEN' }
+        end
+        
+        describe "getter functions for source_language 'nil'" do
+          before { subject.source_language = nil }
+          its(:term_in_source_language) { should == nil }
+        end
+
+        describe "setter functions on Japanese term for source_language 'ja'" do
+          before { subject.source_language = 'ja' ; subject.term_in_ja = '理研' }
+          its(:term_in_en) { should == 'RIKEN' }
+          its(:term_in_ja) { should == '理研' }
+          its(:term_in_source_language) { should == '理研' }
+        end
+
+        describe "setter functions on English term for source_language 'ja'" do
+          before { subject.source_language = 'ja' ; subject.term_in_en = 'Rikagaku Kenkyujo' }
+          its(:term_in_en) { should == 'Rikagaku Kenkyujo' }
+          its(:term_in_ja) { should == '理化学研究所' }
+          its(:term_in_source_language) { should == '理化学研究所' }
+        end
+
+        describe "setter functions on source_language 'ja'" do
+          before { subject.source_language = 'ja' ; subject.term_in_source_language = '理研' }
+          its(:term_in_en) { should == 'RIKEN' }
+          its(:term_in_ja) { should == '理研' }
+          its(:term_in_source_language) { should == '理研' }
+        end
+
+      end
+
+    end
+
+  end
+
+end
