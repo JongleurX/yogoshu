a3f9143fc7188b0cd3bfe2e0a696e6aa358ff34a
Chris Salzberg <chrissalzberg@gmail.com>
2012-01-07 13:15:51 +0900

    finished scenario for user delete

--- features/step_definitions/user_account_steps.rb
+++ features/step_definitions/user_account_steps.rb
@@ -21,6 +21,18 @@ When /^I add the following user:$/ do |table|
   click_on("Create")
 end
 
+When /^I (delete) the user "([^"]*)"$/ do |action,username|
+  visit users_path
+  within('table') do
+    row = find(:xpath, "//tr[./td[contains(.,\"#{username}\")]]")
+    row.find('a', :text => action).click
+  end
+end
+
+When /^I confirm the delete$/ do
+  pending
+end
+
 Then /^I should be logged in as "([^"]*)"$/ do |name|
   @user = User.find_by_name(name)
   page.should have_link('Profile', :href => user_path(@user))
@@ -39,12 +51,14 @@ Then /^I should see the profile page for "([^"]*)"$/ do |name|
   page.should have_xpath('//title', :text => "Yogoshu: #{name}")
 end
 
-#Then /^user "([^"]*)" with password "([^"]*)" should exist$/ do |name,password|
-#  @user = User.find_by_name(name)
-#  @user.should_not be_nil
-#  @user.authenticated?(password).should == true
-#end
-
-Then /^there should be (?:only |)(\d+) users$/ do |count|
+Then /^there should be (?:only |)(\d+) users?$/ do |count|
   User.count.should == count.to_i
+end
+
+Then /^user "([^"]+)" should exist$/ do |name|
+  User.find_by_name(name).should_not be_nil
+end
+
+Then /^user "([^"]+)" should not exist$/ do |name|
+  User.find_by_name(name).should be_nil
 end
--- features/user_delete.feature
+++ features/user_delete.feature
@@ -5,15 +5,13 @@ Feature: Delete user account
 
   Background:
     Given the following users:
-      | name  | password | role         |
-      | jens  | secret   | contributor  |
-      | susan | secret   | manager      |
-    And I am logged in as "susan"
+      | name  | role         |
+      | jens  | contributor  |
+      | susan | manager      |
 
-  @wip
-  Scenario: Successful user delete
-    Given I am on jens' profile page
-    When I click "delete user"
-    Then I should be asked to confirm the delete
-    And jens' account should be deleted
+  Scenario: Glossary manager successfully deletes user
+    Given I am logged in as "susan"
+    When I delete the user "jens"
+    #And I confirm the delete
+    Then user "jens" should not exist
     And there should be only 1 user
