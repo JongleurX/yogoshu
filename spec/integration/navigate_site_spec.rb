79a51fd6e28807c36431508ac4a5ca5fc747324d
Chris Salzberg <chrissalzberg@gmail.com>
2012-07-09 14:19:46 +0900

    move login step to separate function and call from test to avoid problem with sessions, ref: http://stackoverflow.com/questions/4738859/maintaining-session-with-capybara-and-rails-3

--- spec/integration/navigate_site_spec.rb
+++ spec/integration/navigate_site_spec.rb
@@ -2,25 +2,30 @@ require 'spec_helper'
 
 describe "Site navigation" do
 
+  def login(username, password)
+    visit login_path
+    fill_in "User name", :with => username
+    fill_in "Password", :with => password
+    click_on "Login"
+  end
+
   before do
     I18n.locale = 'en'
     @entry = FactoryGirl.create(:entry)
     FactoryGirl.create(:manager)
-    visit login_path
-    fill_in "User name", :with => "manager_user"
-    fill_in "Password", :with => "secret"
-    click_on "Login"
   end
 
   describe "navigate to the edit page for an entry" do
 
     it "links to edit page from entry" do
+      login("manager_user", "secret")
       visit entry_path(@entry)
       click_link "Edit"
       page.should have_css("title", :text => /Edit entry/)
     end
 
     it "links to edit page from entry index" do
+      login("manager_user", "secret")
       visit entries_path
       within(:xpath, "//tr[./td[contains(.,'#{@entry.term_in_glossary_language}')]]") do
         click_link "edit"
