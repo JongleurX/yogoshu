fa6e6934f16c6ee1f7d551eebd8c2b9818410aac
Chris Salzberg <chrissalzberg@gmail.com>
2012-09-13 15:16:45 +0900

    add search button id and use it when clicking links in cucumber tests.

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,7 +1,7 @@
 When /^I search for the keywords? "([^"]*)"$/ do |keywords|
   visit homepage_path
   fill_in "search", :with => keywords
-  click_on "Submit"
+  click_on "search-button"
 end
 
 ["should", "should not"].each do |condition|
